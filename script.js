e5967adced63edf69d9251d14858ca9ecc54b7e3
Cristianv9 <156293750+Cristianv9@users.noreply.github.com>
2025-02-04 19:12:26 +0000

    added js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-
-console.log('👍🏻 okay');
+const body = document.querySelector('body');
+console.log(body);
+body.style.backgroundColor = 'red';
+body.style.backgroundColor = colorkit.random();
+colorkit.sparkle(body);
